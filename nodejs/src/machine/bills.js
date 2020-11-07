69180237541f49550b0f866916f7761ae072efbf
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2020-11-07 18:08:30 +0530

    Progress bill dialog

--- nodejs/src/machine/bills.js
+++ nodejs/src/machine/bills.js
@@ -125,8 +125,9 @@ const bills = {
                 isValid: isValid,
                 messageContent: parsedInput
               }
-              console.log(context.message);
-              context.bills.slots.service = '';
+              if(isValid) {
+                context.bills.slots.service = parsedInput;
+              }
             }),
             always: [
               {
@@ -149,10 +150,109 @@ const bills = {
       },
       searchParamOptions: {
         id: 'searchParamOptions',
-        onEntry: assign((context, event) => {
-          context.chatInterface.toUser(context.user, 'Yet to be implemented');
-        }),
-        always: '#endstate'
+        initial: 'question',
+        states: {
+          question: {
+            onEntry: assign((context, event) => {
+              context.maxValidEntry = 3;
+              let message = 'Please type and send the number of your option from the list given 👇 below:\n1. Search 🔎 using another Mobile No.📱\n2. Search 🔎 using Connection No.\n3. Search 🔎 using Consumer ID\n\nOr Type and send \'mseva\' to Go ⬅️ Back to main menu.';
+              context.chatInterface.toUser(context.user, message);
+            }),
+            on: {
+              USER_MESSAGE: 'process'
+            },
+          },
+          process: {
+            onEntry: assign((context, event) => {
+              var parsedInput = parseInt(event.message.input.trim());
+              var isValid = parsedInput > 0 && parsedInput <= context.maxValidEntry;
+              context.message = {
+                isValid: isValid,
+                messageContent: parsedInput
+              }
+              if(isValid) {
+                var searchParamOption = ''
+                if(parsedInput === 1)
+                  searchParamOption = 'Mobile Number'
+                else if(parsedInput === 2)
+                  searchParamOption = 'Connection Number'
+                else
+                  searchParamOption = 'Consumer ID'
+                context.bills.slots.searchParamOption = searchParamOption;
+              }
+            }),
+            always: [
+              {
+                target: 'error',
+                cond: (context, event) => !context.message.isValid
+              },
+              {
+                target: '#paramInput'
+              }  
+            ]
+          },
+          error: {
+            onEntry: assign((context, event) => {
+              let message = 'Sorry, I didn\'t understand. Could please try again entering a number for the given options.';
+              context.chatInterface.toUser(context.user, message);
+            }),
+            always: 'question'
+          }
+        }
+      },
+      paramInput: {
+        id: 'paramInput',
+        initial: 'question',
+        states: {
+          question: {
+            onEntry: assign((context, event) => {
+              context.chatInterface.toUser(context.user, 'Please Enter ' + context.bills.slots.searchParamOption + ' to view the bill.');
+            }),
+            on: {
+              USER_MESSAGE: 'process'
+            }
+          },
+          process: {
+            onEntry: assign((context, event) => {
+              let message = {
+                isValid: true,
+                messageContent: event.message.input
+              };
+              context.message = message;
+              if(message.isValid) {
+                context.bills.slots.paramInput = message.messageContent
+              }
+            }),
+            always: '#listOfBills'
+          }
+        }
+      },
+      listOfBills: {
+        id: 'listOfBills',
+        invoke: {
+          id: 'fetchBillsForParam',
+          src: (context, event) => {
+            let slots = context.bills.slots;
+            return billService.fetchBillsForParam(context.user, slots.service, slots.searchParamOptions, slots.paramInput);
+          },
+          onDone: [
+            {
+              cond: (context, event) => event.data.pendingBills,
+              actions: assign((context, event) => {
+                let message = 'Your Water 🚰 and Sewerage bill against consumer number WS123456 for property in Azad Nagar, Amritsar for the period Apr-June 2020 is Rs. 630. \n\nPay before 25th July 2020 to avoid late payment charges. \n\nPayment Link: www.mseva.gov.in/cpay/WSbill/WS123456';
+                context.chatInterface.toUser(context.user, message);
+              }),
+              target: '#searchBillInitiate'
+            },
+            {
+              actions: assign((context, event) => {
+                let message = 'The ' + context.bills.slots.searchParamOption + ': ' + context.bills.slots.paramInput + ' is not found in our records. Please Check the details you have provided once again.';
+                context.chatInterface.toUser(context.user, message);
+              }),
+              target: '#searchBillInitiate'
+            }
+          ]  
+        }
       }
     }
 };
