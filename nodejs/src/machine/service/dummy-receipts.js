da051a4d46288130d25c15e5df2656ed1c0f20ff
li8max <sivadatta1204@gmail.com>
2020-11-13 18:33:54 +0530

    updated with all files

---
+++ nodejs/src/machine/service/dummy-receipts.js
@@ -0,0 +1,28 @@
+const receipts = require("../receipts");
+
+class DummyReceipts{
+    async findreceipts(user){
+        let receipts = [
+            {
+                date: '23/10/2020',
+                service: 'WS',
+                charges: 'Rs. 100'
+            },
+            {
+                date: '23/10/2020',
+                service: 'PT',
+                charges: 'Rs. 100'
+            }
+        ]
+
+        let emptyReceipts = []
+
+        return receipts;
+    }
+    async fetchReceiptsForParam(user, menu, searchparams, paraminput) {
+        console.log(`Received params: ${user}, ${menu}, ${searchparams}, ${paraminput}`);
+        return this.findreceipts(user);
+    }
+
+}
+module.exports = new DummyReceipts();
