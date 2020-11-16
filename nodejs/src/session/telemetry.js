8a0b3569b7795514573c8fa4299f3f2a20d27b5b
Madhavan Thirumalai <mthirumalai@Madhavans-MacBook-Air-2.local>
2020-11-16 16:47:23 -0500

    Telemetry progress

--- nodejs/src/session/telemetry.js
+++ nodejs/src/session/telemetry.js
@@ -1,7 +1,52 @@
 class Telemetry {
     log(userId, type, data) {
-        console.log(`[Telemetry: Date: ${new Date()}, User: ${userId}, Type: ${type}, Data:<${data}>]`);
+        let object = {
+            date: new Date().getTime(),
+            user: userId,
+            type: type,
+            data: data
+        }
+        console.log("-----------");
+        console.log(JSON.stringify(object));
+        console.log("-----------");
     }
 }
+
+// TODO from_user message, data paremeter is a JSON object. Elasticsearch is expecting a string
+
+// TODO transition message, ... ditto
+// {"date":1605536890093,"user":"919428010072","type":"transition","data":"{oboarding,oboarding.onboardingName,oboarding.onboardingName.question}"}
+// "reason": "object mapping for [data] tried to parse field [data] as object, but found a concrete value"
+
+// async fetchMdmsData(tenantId, moduleName, masterName, filterPath) {
+//     var mdmsHost = config.mdmsHost;
+//     var url = mdmsHost + 'egov-mdms-service/v1/_search';
+//     var request = {
+//       "RequestInfo": {},
+//       "MdmsCriteria": {
+//         "tenantId": tenantId,
+//         "moduleDetails": [
+//           {
+//             "moduleName": moduleName,
+//             "masterDetails": [
+//               {
+//                 "name": masterName,
+//                 "filter": filterPath
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     };
+  
+//     var options = {
+//       method: 'POST',
+//       body: JSON.stringify(request),
+//       headers: {
+//           'Content-Type': 'application/json'
+//       }
+//     }
+  
+//     let response = await fetch(url, options);
 
 module.exports = new Telemetry();
