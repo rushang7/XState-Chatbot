a974b691b24f146df6eded36c88089b0b98ea42a
praneeth-egov <praneeth.reddy@egovernments.org>
2020-12-17 11:26:00 +0530

    RAIN-1811 Camelcase in environment variables

--- nodejs/src/env-variables.js
+++ nodejs/src/env-variables.js
@@ -1,42 +1,42 @@
 const os = require('os');
 
 const envVariables = {
-    serviceId : process.env.name || 'xstate-chatbot-server',
-    ver : process.env.version || '0.0.1',
+    serviceId : process.env.NAME || 'xstate-chatbot-server',
+    ver : process.env.VERSION || '0.0.1',
 
-    port: process.env.service_port || 8080,
-    contextPath : process.env.contextPath || '/',
+    port: process.env.SERVICE_PORT || 8080,
+    contextPath : process.env.CONTEXT_PATH || '/',
 
-    whatsAppProvider: process.env.whatsAppProvider || 'console',
+    whatsAppProvider: process.env.WHATSAPP_PROVIDER || 'console',
 
-    serviceProvider: process.env.serviceProvider || 'dummy',
+    serviceProvider: process.env.SERVICE_PROVIDER || 'dummy',
 
-    repoProvider: process.env.repoProvider || 'PostgreSQL',
+    repoProvider: process.env.REPO_PROVIDER || 'PostgreSQL',
 
-    mdmsHost: process.env.mdmsHost || 'https://egov-micro-dev.egovernments.org/',
+    mdmsHost: process.env.MDMS_HOST || 'https://egov-micro-dev.egovernments.org/',
 
-    localisationServiceHost: process.env.localisationServiceHost || 'https://egov-micro-dev.egovernments.org/',
-    localisationServiceSearchPath: process.env.localisationServiceSearchPath || 'localization/messages/v1/_search',
+    localisationServiceHost: process.env.LOCALISATION_SERVICE_HOST || 'https://egov-micro-dev.egovernments.org/',
+    localisationServiceSearchPath: process.env.LOCALISATION_SERVICE_SEARCH_PATH || 'localization/messages/v1/_search',
 
-    rootTenantId: process.env.rootTenantId || 'pb',
+    rootTenantId: process.env.ROOT_TENANTID || 'pb',
 
-    supportedLocales: process.env.supportedLocales || 'en_IN,hi_IN',
+    supportedLocales: process.env.SUPPORTED_LOCALES || 'en_IN,hi_IN',
 
-    externalHost: process.env.externalHost || 'https://egov-micro-dev.egovernments.org/',
+    externalHost: process.env.EXTERNAL_HOST || 'https://egov-micro-dev.egovernments.org/',
 
-    cityExternalWebpagePath: process.env.cityExternalWebpagePath || 'citizen/openlink/whatsapp/city',
-    localityExternalWebpagePath: process.env.localityExternalWebpagePath || 'citizen/openlink/whatsapp/locality',
+    cityExternalWebpagePath: process.env.CITY_EXTERNAL_WEBPAGEPATH || 'citizen/openlink/whatsapp/city',
+    localityExternalWebpagePath: process.env.LOCALITY_EXTERNAL_WEBPAGEPATH || 'citizen/openlink/whatsapp/locality',
 
-    whatsAppBusinessNumber : process.env.whatsAppBusinessNumber || '917834811114',
+    whatsAppBusinessNumber : process.env.WHATSAPP_BUSINESS_NUMBER || '917834811114',
 
-    userServiceHost: process.env.userServiceHost || 'https://egov-micro-dev.egovernments.org/',
-    userServiceOAuthPath: process.env.userServiceOAuthPath || 'user/oauth/token',
-    userServiceCreateCitizenPath: process.env.userServiceCreateCitizenPath || 'user/citizen/_create',
-    userServiceUpdateProfilePath: process.env.userServiceUpdateProfilePath || 'user/profile/_update',
-    userServiceHardCodedPassword: process.env.userServiceHardCodedPassword || '123456',
-    userLoginAuthorizationHeader: process.env.userLoginAuthorizationHeader || 'Basic ZWdvdi11c2VyLWNsaWVudDplZ292LXVzZXItc2VjcmV0',
+    userServiceHost: process.env.USER_SERVICE_HOST || 'https://egov-micro-dev.egovernments.org/',
+    userServiceOAuthPath: process.env.USER_SERVICEO_AUTHPATH || 'user/oauth/token',
+    userServiceCreateCitizenPath: process.env.USER_SERVICE_CREATE_CITIZENPATH || 'user/citizen/_create',
+    userServiceUpdateProfilePath: process.env.USER_SERVICE_UPDATE_PROFILEPATH || 'user/profile/_update',
+    userServiceHardCodedPassword: process.env.USER_SERVICE_HARDCODEDPASSWORD || '123456',
+    userLoginAuthorizationHeader: process.env.USER_LOGIN_AUTHORIZATION_HEADER || 'Basic ZWdvdi11c2VyLWNsaWVudDplZ292LXVzZXItc2VjcmV0',
 
-    googleAPIKey: process.env.googleAPIKey || ''
+    googleAPIKey: process.env.GOOGLE_APIKEY || ''
 }
 
 module.exports = envVariables;
