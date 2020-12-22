c1a3b7c16e60ce74204afb9688f739f64c13137e
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2020-12-22 11:27:33 +0530

    Changed schema name. Need to reconstruct the DB.

--- nodejs/db/migration/V20201016131300__chat.sql
+++ nodejs/db/migration/V20201016131300__chat.sql
@@ -1,10 +1,10 @@
-CREATE TABLE chat_state(
+CREATE TABLE eg_chat_state_v2 (
     id SERIAL,
-    user_id character varying(100),
+    user_id TEXT,
     active BOOLEAN,
     state jsonb,
     PRIMARY KEY (id)
 );
 
-CREATE INDEX chat_state_idx_user_id ON chat_state (user_id);
-CREATE INDEX chat_state_idx_active ON chat_state (active);
+CREATE INDEX IF NOT EXISTS eg_chat_state_idx_user_id_v2 ON eg_chat_state_v2 (user_id);
+CREATE INDEX IF NOT EXISTS eg_chat_state_idx_active_v2 ON eg_chat_state_v2 (active);
