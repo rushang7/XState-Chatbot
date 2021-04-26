e37176d3790472880e576cc8addeb0cb7bfc89e1
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-04-26 14:12:36 +0530

    Expose GET endpoint for the webhook

--- nodejs/src/channel/routes/index.js
+++ nodejs/src/channel/routes/index.js
@@ -5,6 +5,14 @@ const express = require('express'),
     channelProvider = require('../');
 
 router.post('/message', async (req, res) =>  {
+    processHttpRequest(req, res);
+});
+
+router.get('/message', async (req, res) =>  {
+    processHttpRequest(req, res);
+});
+
+async function processHttpRequest(req, res) {
     try {
         let reformattedMessage = await channelProvider.processMessageFromUser(req);
         if(reformattedMessage)
@@ -13,7 +21,7 @@ router.post('/message', async (req, res) =>  {
         console.log(e);
     }
     res.end();
-});
+}
 
 router.get('/health', (req, res) => res.sendStatus(200));
 
